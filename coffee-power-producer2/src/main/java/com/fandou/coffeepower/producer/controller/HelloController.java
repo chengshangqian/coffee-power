ceaef4efff5d9d4bd874fffacb8355d8de011e5e
chengshangqian <chengshangqian@126.com>
2020-10-27 21:50:05 +0800

    测试服务消费方负载均衡调用：服务提供方返回被调用的端口号信息

--- coffee-power-producer2/src/main/java/com/fandou/coffeepower/producer/controller/HelloController.java
+++ coffee-power-producer2/src/main/java/com/fandou/coffeepower/producer/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.fandou.coffeepower.producer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "hello Mango2 !";
+        return String.format("{}:{} => Hello, Coffee!",applicationName,serverPort);
     }
 }
